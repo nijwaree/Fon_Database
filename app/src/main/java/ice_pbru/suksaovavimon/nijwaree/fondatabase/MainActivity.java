0c2a18327b18e2367943ccfc9230b5c7b4ed11ce
Jaruwan <jatii.z77@gmail.com>
2016-04-26 11:52:54 +0700

    Finish design signup screen and bg

--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/MainActivity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/MainActivity.java
@@ -1,13 +1,46 @@
 package ice_pbru.suksaovavimon.nijwaree.fondatabase;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
+
+    // Explicit
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // bindwidget
+
+        bindwidget();
+
+
+
+
+
+
+
+
+    } // main method
+
+    private void bindwidget() {
+
     }
-}
+
+    public void clickRegis(View view) {
+        startActivity(new Intent(MainActivity.this, SignUp.class));
+
+
+    }
+
+
+
+
+
+} // main class
