2da2b13e6f1769565f077322aee60e347dd0367d
Jaruwan <jatii.z77@gmail.com>
2016-04-26 10:43:24 +0700

    Finish design signup screen

---
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/SignUp.java
@@ -0,0 +1,13 @@
+package ice_pbru.suksaovavimon.nijwaree.fondatabase;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SignUp extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+    }
+}
