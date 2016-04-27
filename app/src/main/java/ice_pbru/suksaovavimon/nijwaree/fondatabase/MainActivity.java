eec09d75352f458b4d85439329075ff5cb72631f
Jaruwan <jatii.z77@gmail.com>
2016-04-27 15:40:15 +0700

    Finish signin

--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/MainActivity.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/MainActivity.java
@@ -4,10 +4,15 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
     // Explicit
+
+    private EditText nameEditText, passwoedEditText;
+
+    private String nameString, passwordString;
 
 
 
@@ -29,9 +34,37 @@ public class MainActivity extends AppCompatActivity {
 
     } // main method
 
-    private void bindwidget() {
+
+    public void clickSignIn(View view) {
+        nameString = nameEditText.getText().toString().trim();
+        passwordString = passwoedEditText.getText().toString().trim();
+
+        if (checkspace()) {
+            // Have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณาตรวจสอบข้อมูลให้ครบถ้วน");
+
+
+        } else {
+            // no space
+
+        }
+
+
+    } // clickSignIn
+
+    private boolean checkspace() {
+
+        return nameString.equals("")||passwordString.equals("");
 
     }
+
+    private void bindwidget() {
+        nameEditText = (EditText) findViewById(R.id.editText6);
+        passwoedEditText = (EditText) findViewById(R.id.editText7);
+
+
+    } // bindwidget
 
     public void clickRegis(View view) {
         startActivity(new Intent(MainActivity.this, SignUp.class));
--- app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/SignUp.java
+++ app/src/main/java/ice_pbru/suksaovavimon/nijwaree/fondatabase/SignUp.java
@@ -2,12 +2,76 @@ package ice_pbru.suksaovavimon.nijwaree.fondatabase;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class SignUp extends AppCompatActivity {
+
+    // Explicit
+
+    private EditText nameEditText, surnameEditText, userEditText, passwordEditText, emailEditText;
+
+    private String nameString, surnameString, userString, passwordString, emailString;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+
+
+        // bindwidget
+
+        bindwidget();
+
+
+
+    } // Main method
+
+
+    public void clickSignUp (View view) {
+        nameString = nameEditText.getText().toString().trim();
+        surnameString = surnameEditText.getText().toString().trim();
+        userString = userEditText.getText().toString().trim();
+        passwordString = passwordEditText.getText().toString().trim();
+        emailString = emailEditText.getText().toString().trim();
+
+
+        if (checkspace()) {
+            // Have space
+            MyAlert myAlert = new MyAlert();
+            myAlert.myDialog(this, "มีช่องว่าง", "กรุณาตรวจสอบข้อมูลและกรอกให้ครบถ้วน");
+
+
+        } else {
+            // No space
+
+        }
+
+    } // clickSignUp
+
+    private boolean checkspace() {
+
+        return nameString.equals("")||
+                surnameString.equals("")||
+                userString.equals("")||
+                passwordString.equals("")||
+                emailString.equals("");
+
+
     }
-}
+
+
+    private void bindwidget() {
+        nameEditText = (EditText) findViewById(R.id.editText);
+        surnameEditText = (EditText) findViewById(R.id.editText2);
+        userEditText = (EditText) findViewById(R.id.editText3);
+        passwordEditText = (EditText) findViewById(R.id.editText4);
+        emailEditText = (EditText) findViewById(R.id.editText5);
+
+
+    } // bindwidget
+
+
+} // Main class
